776e6eae2afac3393e19e1670dfc335cee3a241c
Frabrizio Velazquez <frabrizio@Frabrizios-Mac-mini.local>
2026-09-17 15:08:08 -0700

    Force BAC water image cache refresh with version param

--- shop_products_data.js
+++ shop_products_data.js
@@ -348,7 +348,7 @@ const VX_PRODUCTS = {
             purity: '',
             price: 12,
             cogs: 3.50,
-            image: 'images/products/bac-water-10ml.svg',
+            image: 'images/products/bac-water-10ml.svg?v=2',
             url: '#',
             inStock: true,
             dualTested: false,
