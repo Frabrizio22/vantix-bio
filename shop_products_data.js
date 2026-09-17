26c9a517b3bc0b18dd42dbd13ceed0d82425cafd
Frabrizio Velazquez <frabrizio@Frabrizios-Mac-mini.local>
2026-09-17 15:09:35 -0700

    BAC water: new filename to force browser cache refresh

--- shop_products_data.js
+++ shop_products_data.js
@@ -348,7 +348,7 @@ const VX_PRODUCTS = {
             purity: '',
             price: 12,
             cogs: 3.50,
-            image: 'images/products/bac-water-10ml.svg?v=2',
+            image: 'images/products/bac-water-10ml-v2.svg',
             url: '#',
             inStock: true,
             dualTested: false,
